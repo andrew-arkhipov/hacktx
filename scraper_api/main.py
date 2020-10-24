2f216b177947e589b12403713acdcc848ff0f7db
philipk19238 <pkung67@utexas.edu>
2020-10-24 17:44:45 -0500

    bug fixes

--- scraper_api/main.py
+++ scraper_api/main.py
@@ -1,6 +1,16 @@
 from bs4 import BeautifulSoup 
 import requests
 
+class Furniture: 
+
+    def __init__(self, title, href, price):
+        self.title = title 
+        self.href = href
+        self.price = price
+
+    def __repr__(self):
+        return self.title + '\n' + self.href + '\n' + self.price
+
 class Scraper:
 
     def __init__(self, html):
@@ -12,8 +22,8 @@ class Scraper:
     def find_listings(self):
         res = []
         for listing in self.soup.findAll('li', {'class':['result-row']}):
-            title, href, price = Furniture(title, href, price)
-            res.append(Furniture)
+            title, href, price = self.get_listing_info(listing)
+            res.append(Furniture(title, href, price))
         return res
 
     def get_listing_info(self, listing):
@@ -25,21 +35,12 @@ class Scraper:
     
         return title_text, post_href, price
 
-class Furniture: 
-
-    def __init__(self, title, href, price):
-        self.title = title 
-        self.href = href
-        self.price = price
-
-    def __repr__(self):
-        print(self.title + '\n' + self.href + '\n' + self.price)
-
 def main(url):
     html = requests.get(url).text
     scraper = Scraper(html)
     info = scraper.find_listings()
     print(info)
 
-
-    
+if __name__ == '__main__':
+    url = 'https://austin.craigslist.org/search/fua?query=bed'
+    main(url)

c77cf15dd121c6b7e643a5a2e4f2b05438b4ed86
philipk19238 <pkung67@utexas.edu>
2020-10-24 18:17:52 -0500

    adding body scrape

--- scraper_api/main.py
+++ scraper_api/main.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup 
 import requests
+import time
 
-class Furniture: 
+class Listing: 
 
-    def __init__(self, title, href, price):
+    def __init__(self, title, href, price, time):
         self.title = title 
         self.href = href
         self.price = price
+        self.time = time
 
     def __repr__(self):
         return self.title + '\n' + self.href + '\n' + self.price
@@ -19,8 +21,8 @@ class Scraper:
     def find_listings(self):
         res = []
         for listing in self.soup.findAll('li', {'class':['result-row']}):
-            title, href, price = self.get_listing_info(listing)
-            res.append(Furniture(title, href, price))
+            title, href, price, time = self.get_listing_info(listing)
+            res.append(Listing(title, href, price, time))
         return res
 
     def get_listing_info(self, listing):
@@ -30,7 +32,12 @@ class Scraper:
         time = listing.find('time', {'class':['result-date']})['title']
         price = listing.find('span', {'class':['result-price']}).text
     
-        return title_text, post_href, price
+        return title_text, post_href, price, time
+
+    def get_listing_body(self):
+        body = self.soup.find('section', {'id':['postingbody']})
+        return body.text
+
 
 def main(url):
     html = requests.get(url).text
@@ -39,5 +46,21 @@ def main(url):
     print(info)
 
 if __name__ == '__main__':
+    start = time.time()
     url = 'https://austin.craigslist.org/search/fua?query=bed'
-    main(url)
+    html = requests.get(url).text
+    scraper = Scraper(html)
+    listing_array = scraper.find_listings()
+
+    body = []
+
+    for listing in listing_array: 
+        url = listing.href
+        html = requests.get(url).text
+        scraper = Scraper(html)
+        body.append(scraper.get_listing_body())
+        
+    end = time.time()
+    print(end - start)
+        
+    

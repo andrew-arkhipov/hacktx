3d6110e7a130c31b1adfcc8e272b38931fb82dc7
philipk19238 <pkung67@utexas.edu>
2020-10-25 06:42:43 -0500

    finishing up landing page and enter info UI

--- scraper_api/app/routes.py
+++ scraper_api/app/routes.py
@@ -12,28 +12,29 @@ def enter_info():
     return render_template('enterInfo.html')
 
 
-@app.route('/getFurniture' methods=['GET', 'POST'])
+@app.route('/furniture_class', methods=['GET', 'POST'])
 def get_furniture():
     if request.method == 'POST':
         user_json = request.get_json()
-        furniture_array = user_json["furniture_array"]
+        furniture_array = user_json["elements"]
         city = user_json["city"]
         zip_code = user_json["zip_code"]
 
+        print(user_json)
+        return render_template('landing.html')
 
-@app.route('/getJobs', methods=['GET', 'POST'])
+@app.route('/job_class', methods=['GET', 'POST'])
 def get_jobs():
     if request.method == 'POST':
         pass
 
 
-@app.route('/getHousing' methods=['GET', 'POST'])
+@app.route('/housing_class', methods=['GET', 'POST'])
 def get_housing():
     if request.method == 'POST':
-        pass 
+        pass
 
-
-@app.route('/getAll' methods=['GET', 'POST'])
+@app.route('/getAll', methods=['GET', 'POST'])
 def get_all():
     if request.method == 'POST':
         pass

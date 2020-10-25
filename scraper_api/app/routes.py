8fdd55ba15d7a77f4ea3c9d408bd0bf9c3095a9c
philipk19238 <pkung67@utexas.edu>
2020-10-25 00:33:03 -0500

    adding UI elements

--- scraper_api/app/routes.py
+++ scraper_api/app/routes.py
@@ -1,13 +1,38 @@
 from app import app 
-from flask import render_template
+from flask import render_template, request
+from app.utils import scraper
 
 @app.route('/')
 def home():
-    return 'hello world'
+    return render_template("landing.html")
 
 
 @app.route('/enterInfo')
 def enter_info():
     return render_template('enterInfo.html')
+
+
+@app.route('/processInfo')
+def process_info():
+    if request.method == 'POST':
+        user_json = request.get_json()
+        location = user_json["location"]
+        family_number = user_json["family_number"]
+        income = user_json["income"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
